db94886a5a556c6c71ee0e4d183ad6054ea81b4d
FakeYou <ag.nanninga@gmail.com>
2014-04-02 12:03:58 +0200

    broke multiply

--- src/main/java/com/kratos/app/Calculator.java
+++ src/main/java/com/kratos/app/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
     }
 
     public static double multiply(double number1, double number2) {
-        return number1 * number2;
+        return number1 * number1;
     }
 
     public static double divide(double number1, double number2) {
